f3d5141aea441e6535cd8939f25fc49e26a05517
davizassu <filipehdavi@gmail.com>
2026-09-20 18:09:02 -0300

    Add Portugol version of time converter

--- 03_conversor_tempo.py
+++ 03_conversor_tempo.py
@@ -7,3 +7,21 @@ minutos = (total_segundos % 3600) // 60
 segundos = total_segundos % 60
 
 print(f"{total_segundos} segundos = {horas}h {minutos}min {segundos}s")
+
+#Versão Portugol:
+programa {
+  funcao inicio() {
+    inteiro total_segundos, horas, minutos, segundos
+
+    // Lê o tempo total em segundos
+    escreva("Digite o tempo em segundos: ")
+    leia(total_segundos)
+
+    // Divisão inteira (/) e resto (%) separam horas, minutos e segundos
+    horas = total_segundos / 3600
+    minutos = (total_segundos % 3600) / 60
+    segundos = total_segundos % 60
+
+    escreva(total_segundos, " segundos = ", horas, "h ", minutos, "min ", segundos, "s\n")
+  }
+}
