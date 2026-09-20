51c2912221dd1fc6d317117176699033633efa90
davizassu <filipehdavi@gmail.com>
2026-09-20 15:11:21 -0300

    Add files via upload

---
+++ 02_media_notas.py
@@ -0,0 +1,9 @@
+"""Desafio 2 - Média de notas."""
+
+nota_1 = float(input("Digite a 1ª nota: "))
+nota_2 = float(input("Digite a 2ª nota: "))
+nota_3 = float(input("Digite a 3ª nota: "))
+
+media = (nota_1 + nota_2 + nota_3) / 3
+
+print(f"Média: {media:.2f}")
