70e788a6cb7c068a909640350906c0c508eaf4d9
davizassu <filipehdavi@gmail.com>
2026-09-20 15:27:40 -0300

    Enhance code readability with comments
    Added comments to clarify the code functionality.

--- 01_calculadora_troco.py
+++ 01_calculadora_troco.py
@@ -1,6 +1,13 @@
 valor_compra = float(input("Valor da compra: R$ "))
+#Lê o valor digitado pelo usuário e converte em ponto flutuante
 valor_pago = float(input("Valor pago: R$ "))
 
 troco = valor_pago - valor_compra
-
+#subtrai os valores dados e guarda na variável "troco"
 print(f"Troco: R$ {troco:.2f}")
+#imprime o resultado com a formatação de 2 casas decimais ".2f". 
+#Isso ocorre para que não apareça números muito longos na tela
+
+
+
+
